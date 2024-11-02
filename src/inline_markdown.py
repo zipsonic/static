f28b312fc7eb6424f655ea78660db56fe81bc14e
zipsonic <rick@hail3.com>
2024-11-02 16:43:32 -0700

    fixed bug in block segration edge case

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -103,14 +103,14 @@ def markdown_to_blocks(markdown):
 
         line2add = markdownlines[counter].strip()
 
-        if len(markdownlines[counter]) == 0:
+        if len(line2add) == 0:
             counter += 1
             continue
-        if line2add[0] == "*" and (line2add.count("*") % 2 == 1):
+        if len(line2add) > 1 and line2add[0:2] == "* ":
             ismarkdownlist = True
         else:
             ismarkdownlist = False
-        if ismarkdownlist and markdownblocks[-1][0] == "*":
+        if ismarkdownlist and len(markdownblocks[-1]) > 1 and markdownblocks[-1][0:2] == "* ":
             line2add = markdownblocks[-1] + "\n" + line2add
             markdownblocks.pop()
 
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -183,5 +183,30 @@ class TestInlineMarkdown(unittest.TestCase):
         
         self.assertListEqual(markdown_to_blocks(testmarkdown),resultblocks)
 
+    def test_markdown_to_blocks(self):
+        testmarkdown = """# This is a heading
+
+            This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+            * This is the first list item in a list block
+            * This is a list item
+            * This is another list item
+            
+            *This* is another sentence at the end to test
+            
+            * A single item list"""
+        
+        resultblocks = [
+            "# This is a heading",
+            "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+            """* This is the first list item in a list block
+* This is a list item
+* This is another list item""",
+            "*This* is another sentence at the end to test",
+            "* A single item list"
+            ]
+        
+        self.assertListEqual(markdown_to_blocks(testmarkdown),resultblocks)
+
 if __name__ == "__main__":
     unittest.main()
