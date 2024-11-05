9b07e834b492d60b1d10c992a617d6d99f134158
zipsonic <rick@hail3.com>
2024-11-05 14:19:53 -0800

    added static to public file copy functionality

--- src/main.py
+++ src/main.py
@@ -1,10 +1,42 @@
+import os
+import shutil
 from textnode import *
 from htmlnode import *
 
+def cleandir(destination):
+
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+
+    if os.path.exists(destination):
+        raise Exception (f"{destination} folder still exists")
+
+    os.mkdir(destination)
+
+    return
+
+def copydir(source, destination):
+
+    cleandir(destination)
+
+    sourcefiles = os.listdir(source)
+
+    for sourcefile in sourcefiles:
+        src = os.path.join(source,sourcefile)
+        dst = os.path.join(destination,sourcefile)
+        if os.path.isfile(src):
+            shutil.copy(src,dst)
+        else:
+            copydir(src,dst)
+    return
 
 def main ():
-    sample = TextNode("This is a text node",TextType.BOLD,"https://www.boot.dev")
-    print (sample)
+    # sample = TextNode("This is a text node",TextType.BOLD,"https://www.boot.dev")
+    # print (sample)
+    # return
+
+    copydir("static","public")
+
     return
 
 main()
